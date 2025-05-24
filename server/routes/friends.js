5930df629d622c384f43a8721f50308857f461cd
Guilherme <gui3miranda@gmail.com>
2025-05-24 18:19:42 +0100

    Alterei a query do api/friends/allUsers, agora só retorna todos os users que ainda não estão na tabela de amizades para esse user e adicionei uma rota que lista todos os users amigos

--- server/routes/friends.js
+++ server/routes/friends.js
@@ -4,22 +4,39 @@ const db = require('../config/db')
 
 // GET ALL USERS EXCEPT USER HIMSELF
 router.get('/allUsers', (req,res) => {
-    const sql = "SELECT concat(name,' ', lastname) as name, public, iduser\n" +
-        "FROM user WHERE iduser != ?\n"
+    const sql = "SELECT concat(name,' ', lastname) as name, public, iduser as idUser\n" +
+        "FROM strava.user\n" +
+        "WHERE iduser != ? AND iduser NOT IN (\n" +
+        "SELECT idreceiver FROM strava.friends WHERE idsender = ?\n" +
+        "UNION\n" +
+        "SELECT idsender FROM strava.friends WHERE idreceiver = ?\n" +
+        ")\n"
 
-    db.query(sql, [req.user.id], (err,result) => {
+    db.query(sql, [req.user.id, req.user.id, req.user.id], (err,result) => {
         if (err) res.status(500).json({error:'Internal server error'})
-        console.log(req.user.id)
         res.status(200).json(result)
     })
 })
 
 // GET ALL FRIENDSHIP REQUESTS FOR 1 USER
 router.get('/requests', (req,res) => {
-    const sql = "SELECT iduser, public, concat(name,' ', lastname) as name\n" +
+    const sql = "SELECT iduser as idUser, public, concat(name,' ', lastname) as name\n" +
         "FROM friends f\n" +
         "JOIN user u ON u.iduser = f.idsender\n" +
         "WHERE state = 'Pendente' and f.idreceiver = ?"
+
+    db.query(sql, [req.user.id], (err,result) => {
+        if (err) res.status(500).json({error:'Internal server error'})
+
+        res.status(200).json(result)
+    })
+})
+
+router.get('/all', (req,res) => {
+    const sql = "SELECT  concat(name,' ', lastname) as name, public, iduser as idUser \n" +
+        "FROM strava.friends f\n" +
+        "JOIN strava.user u ON u.iduser = f.idreceiver\n" +
+        "WHERE idsender = ? AND state = 'Aceite' "
 
     db.query(sql, [req.user.id], (err,result) => {
         if (err) res.status(500).json({error:'Internal server error'})
